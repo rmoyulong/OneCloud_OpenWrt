092f69eaab50973bfcf53ff0a946d056d3ff8904
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-11-30 13:16:25 +0800

    Add files via upload

--- sh/lean1.sh
+++ sh/lean1.sh
@@ -1,53 +1,38 @@
 #!/bin/bash
 
-#Design Theme
-#git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "main" || echo "js") https://github.com/gngpp/luci-theme-design.git
-#git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
-#sed -i 's/dark/light/g' luci-app-design-config/root/etc/config/design
-#Argon Theme
-#git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
-#git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-app-argon-config.git
-#Linkease
-#git clone --depth=1 --single-branch https://github.com/linkease/istore.git
-#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
-#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
-#Open Clash
-#git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
-#Pass Wall
-#git clone --depth=1 --single-branch --branch "main" https://github.com/xiaorouji/openwrt-passwall.git ./pw_luci
-#git clone --depth=1 --single-branch --branch "main" https://github.com/xiaorouji/openwrt-passwall-packages.git ./pw_packages
+# 移除要替换的包
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/msd_lite
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-netgear
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-serverchan
+#rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf package/emortal/default-settings
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
-#预置OpenClash内核和GEO数据
-#export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
-#export CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux
-#export CORE_DEV=https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux
-#export CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux
+#设置armv7 32位的openclash
+mkdir -p files/etc/openclash/core
+mkdir -p files/etc/profile.d
+mkdir -p files/root
 
-#export CORE_TYPE=$(echo $OWRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
-#export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
+CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/dev/clash-linux-armv7.tar.gz"
+CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/dev/premium\?ref\=core | grep download_url | grep armv7 | awk -F '"' '{print $4}')
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/meta/clash-linux-armv7.tar.gz"
+GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+GEO_MMDB_URL"=https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb"
+#kr_URL="https://raw.githubusercontent.com/shiyu1314/openwrt-onecloud/main/sh/1.sh"
+#xx_URL="https://raw.githubusercontent.com/shiyu1314/openwrt-onecloud/main/sh/30-sysinfo.sh"
 
-#export GEO_MMDB=https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb
-#export GEO_SITE=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
-#export GEO_IP=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat
-
-#cd ./OpenClash/luci-app-openclash/root/etc/openclash
-
-#curl -sfL -o ./Country.mmdb $GEO_MMDB
-#curl -sfL -o ./GeoSite.dat $GEO_SITE
-#curl -sfL -o ./GeoIP.dat $GEO_IP
-
-#mkdir ./core && cd ./core
-
-#curl -sfL -o ./tun.gz "$CORE_TUN"-"$CORE_TYPE"-"$TUN_VER".gz
-#gzip -d ./tun.gz && mv ./tun ./clash_tun
-
-#curl -sfL -o ./meta.tar.gz "$CORE_MATE"-"$CORE_TYPE".tar.gz
-#tar -zxf ./meta.tar.gz && mv ./clash ./clash_meta
-
-#curl -sfL -o ./dev.tar.gz "$CORE_DEV"-"$CORE_TYPE".tar.gz
-#tar -zxf ./dev.tar.gz
-
-#chmod +x ./clash* ; rm -rf ./*.gz
+wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
+wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
+wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
+wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+wget -qO- $GEO_MMDB_URL > files/etc/openclash/Country.mmdb
 
 
 # 修改默认IP
@@ -58,16 +43,6 @@
 
 # TTYD 自动登录
 # sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
-
-# 移除要替换的包
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/msd_lite
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-netgear
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/applications/luci-app-netdata
-rm -rf feeds/luci/applications/luci-app-serverchan
 
 # 添加额外插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
