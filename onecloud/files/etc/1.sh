a1f328b0d0d2d75c5ca57f704109c23f2b9ca59f
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-11-30 17:33:45 +0800

    Add files via upload

--- onecloud/files/etc/1.sh
+++ onecloud/files/etc/1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p /mnt/sda1/aria2
+mkdir -p /mnt/sda1/aria2/download
 
 parted /dev/mmcblk1 resizepart 2 100%
 losetup /dev/loop0 /dev/mmcblk1p2
@@ -8,7 +10,6 @@ echo "# Put your custom commands here that should be executed once
 # the system init finished. By default this file does nothing.
 
 mount -o remount rw /
-
 exit 0">/etc/rc.local
 rm -rf /etc/1.sh && reboot
 
