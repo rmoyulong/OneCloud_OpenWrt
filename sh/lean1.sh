33c13f2359fe101c7b01d523e6bb7776c25864e5
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-07 10:25:36 +0800

    优化了ssr设置

--- sh/lean1.sh
+++ sh/lean1.sh
@@ -49,8 +49,8 @@ git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
-svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
-svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-ssr-mudb-server package/luci-app-ssr-mudb-server
+svn export  https://github.com/kenzok8/small-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
+svn export  https://github.com/kenzok8/small-package/trunk/luci-app-ssr-mudb-server package/luci-app-ssr-mudb-server
 svn export https://github.com/kenzok8/small-package/trunk/luci-app-eqos package/luci-app-eqos
 svn export https://github.com/kenzok8/small-package/trunk/luci-app-easymesh package/luci-app-easymesh
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-unblockmusic package/luci-app-unblockmusic
@@ -61,21 +61,25 @@ svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-xunlei pac
 
 # 科学上网插件
 git clone --depth=1 https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
-#git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 svn export https://github.com/haiibo/packages/trunk/luci-app-vssr package/luci-app-vssr
 svn export https://github.com/kenzok8/small-package/trunk/luci-app-bypass package/luci-app-bypass
-svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-passwall package/luci-app-passwall
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-passwall2 package/luci-app-passwall2
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
-svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+#svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
+#svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
 svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 
 # 科学上网插件依赖
 svn so  https://github.com/kiddin9/openwrt-packages/trunk/vlmcsd package/vlmcsd
 svn co https://github.com/kenzok8/small-package/trunk/v2dat package/v2dat
-svn co https://github.com/kenzok8/small-package/trunk/shadow-tls package/shadow-tls
-#svn co https://github.com/kenzok8/small-package/trunk/brook package/brook
+svn co https://github.com/kenzok8/small-package/trunk/shadowsocks-libev package/shadowsocks-libev
+svn co https://github.com/kenzok8/small-package/trunk/brook package/brook
+svn co https://github.com/kenzok8/small-package/trunk/trojan-go package/trojan-go
+svn co https://github.com/kenzok8/small-package/trunk/trojan-plus package/trojan-plus
+svn co https://github.com/kenzok8/small-package/trunk/sing-box package/sing-box
 svn co https://github.com/kenzok8/small-package/trunk/sagernet-core package/sagernet-core
 svn co https://github.com/kenzok8/small-package/trunk/lua-neturl package/lua-neturl
 svn co https://github.com/kenzok8/small-package/trunk/redsocks2 package/redsocks2
@@ -83,7 +87,7 @@ svn co https://github.com/kenzok8/small-package/trunk/UnblockNeteaseMusic-Go pac
 svn co https://github.com/kenzok8/small-package/trunk/UnblockNeteaseMusic package/UnblockNeteaseMusic
 svn co https://github.com/kenzok8/small-package/trunk/aria2 package/aria2
 svn co https://github.com/kenzok8/small-package/trunk/ariang package/ariang
-svn so https://github.com/kiddin9/openwrt-packages/trunk/filebrowser package/filebrowser 
+svn so  https://github.com/kenzok8/small-package/trunk/filebrowser package/filebrowser 
 
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
