9fb0b7e2b051a6598efdbe80c1de3b858beb64f0
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-13 23:55:36 +0800

    一站式和普通image的插件的订阅文件分开建立

--- sh/quick.sh
+++ sh/quick.sh
@@ -10,6 +10,7 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/applications/luci-app-serverchan
 rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-homeproxy
 
 #设置armv7 32位的openclash
 mkdir -p files/etc/openclash/core
@@ -34,67 +35,55 @@ wget -qO- $GEO_MMDB_URL > files/etc/openclash/Country.mmdb
 
 
 # 添加额外插件
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
 git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
-svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
-svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-ssr-mudb-server package/luci-app-ssr-mudb-server
-svn export https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-eqos package/luci-app-eqos
-svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
-svn export https://github.com/NueXini/NueXini_Packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd
-svn export https://github.com/NueXini/NueXini_Packages/trunk/luci-app-qbittorrent package/luci-app-qbittorrent
-svn export https://github.com/kenzok8/small-package/trunk/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-ssr-mudb-server package/luci-app-ssr-mudb-server
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-eqos package/luci-app-eqos
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-easymesh package/luci-app-easymesh
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-ramfree package/luci-app-ramfree
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-kodexplorer package/luci-app-kodexplorer
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-wrtbwmon package/luci-app-wrtbwmon
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-unblockmusic package/luci-app-unblockmusic
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-zerotier package/luci-app-zerotier
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-xunlei package/luci-app-xunlei
 
 # 科学上网插件
+git clone --depth=1 https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 svn export https://github.com/haiibo/packages/trunk/luci-app-vssr package/luci-app-vssr
-svn export https://github.com/kenzok8/small-package/trunk/luci-app-bypass package/luci-app-bypass
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-passwall package/luci-app-passwall
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-passwall2 package/luci-app-passwall2
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
-svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+#svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
+#svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
 svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 
 # 科学上网插件依赖
 svn co https://github.com/kenzok8/small-package/trunk/v2dat package/v2dat
-svn co https://github.com/kenzok8/small-package/trunk/shadow-tls package/shadow-tls
+svn co https://github.com/kenzok8/small-package/trunk/shadowsocks-libev package/shadowsocks-libev
 svn co https://github.com/kenzok8/small-package/trunk/brook package/brook
-svn co https://github.com/kenzok8/small-package/trunk/chinadns-ng package/chinadns-ng
-svn co https://github.com/kenzok8/small-package/trunk/dns2socks package/dns2socks
-svn co https://github.com/kenzok8/small-package/trunk/dns2tcp package/dns2tcp
-svn co https://github.com/kenzok8/small-package/trunk/hysteria package/hysteria
-svn co https://github.com/kenzok8/small-package/trunk/ipt2socks package/ipt2socks
-svn co https://github.com/kenzok8/small-package/trunk/microsocks package/microsocks
-svn co https://github.com/kenzok8/small-package/trunk/naiveproxy package/naiveproxy
-svn co https://github.com/kenzok8/small-package/trunk/pdnsd-alt package/pdnsd-alt
-svn co https://github.com/kenzok8/small-package/trunk/sagernet-core package/sagernet-core
-svn co https://github.com/kenzok8/small-package/trunk/ssocks package/ssocks
-svn co https://github.com/kenzok8/small-package/trunk/tcping package/tcping
 svn co https://github.com/kenzok8/small-package/trunk/trojan-go package/trojan-go
 svn co https://github.com/kenzok8/small-package/trunk/trojan-plus package/trojan-plus
-svn co https://github.com/kenzok8/small-package/trunk/v2ray-geodata package/v2ray-geodata
-svn co https://github.com/kenzok8/small-package/trunk/simple-obfs package/simple-obfs
-svn co https://github.com/kenzok8/small-package/trunk/v2ray-core package/v2ray-core
-svn co https://github.com/kenzok8/small-package/trunk/v2ray-plugin package/v2ray-plugin
-svn co https://github.com/kenzok8/small-package/trunk/shadowsocks-rust package/shadowsocks-rust
-svn co https://github.com/kenzok8/small-package/trunk/shadowsocksr-libev package/shadowsocksr-libev
-svn co https://github.com/kenzok8/small-package/trunk/xray-core package/xray-core
-svn co https://github.com/kenzok8/small-package/trunk/xray-plugin package/xray-plugin
+svn co https://github.com/kenzok8/small-package/trunk/sagernet-core package/sagernet-core
 svn co https://github.com/kenzok8/small-package/trunk/lua-neturl package/lua-neturl
-svn co https://github.com/kenzok8/small-package/trunk/trojan package/trojan
 svn co https://github.com/kenzok8/small-package/trunk/redsocks2 package/redsocks2
-svn co https://github.com/NueXini/NueXini_Packages/trunk/ipv6-helper package/ipv6-helper
-svn co https://github.com/kenzok8/small-package/trunk/UnblockNeteaseMusic-Go package/UnblockNeteaseMusic-Go
-svn co https://github.com/kenzok8/small-package/trunk/UnblockNeteaseMusic package/UnblockNeteaseMusic
 svn co https://github.com/kenzok8/small-package/trunk/aria2 package/aria2
 svn co https://github.com/kenzok8/small-package/trunk/ariang package/ariang
-svn co https://github.com/kenzok8/small-package/trunk/qBittorrent-static package/qBittorrent-static
-svn co https://github.com/NueXini/NueXini_Packages/trunk/vlmcsd package/vlmcsd
-
+svn co https://github.com/kenzok8/small-package/trunk/filebrowser package/filebrowser
+svn co https://github.com/kiddin9/openwrt-packages/trunk/vlmcsd package/vlmcsd
+svn co https://github.com/kiddin9/openwrt-packages/trunk/sing-box package/sing-box
+svn co https://github.com/kiddin9/openwrt-packages/trunk/UnblockNeteaseMusic-Go package/UnblockNeteaseMusic-Go
+svn co https://github.com/kiddin9/openwrt-packages/trunk/UnblockNeteaseMusic package/UnblockNeteaseMusic
+svn co https://github.com/kiddin9/openwrt-packages/trunk/wrtbwmon package/wrtbwmon
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 git clone --depth=1 https://github.com/Joecaicai/luci-theme-ifit package/luci-theme-ifit 
@@ -107,9 +96,9 @@ svn export https://github.com/haiibo/packages/trunk/luci-theme-netgear package/l
 
 # 晶晨宝盒
 svn export https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/haiibo/OpenWrt'|g" package/luci-app-amlogic/root/etc/config/amlogic
+#sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/haiibo/OpenWrt'|g" package/luci-app-amlogic/root/etc/config/amlogic
 # sed -i "s|kernel_path.*|kernel_path 'https://github.com/ophub/kernel'|g" package/luci-app-amlogic/root/etc/config/amlogic
-sed -i "s|ARMv8|ARMv7_PLUS|g" package/luci-app-amlogic/root/etc/config/amlogic
+#sed -i "s|ARMv8|ARMv8_PLUS|g" package/luci-app-amlogic/root/etc/config/amlogic
 
 # SmartDNS
 git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
@@ -136,7 +125,7 @@ svn export https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-
 svn export https://github.com/linkease/istore/trunk/luci package/luci-app-store
 
 # 在线用户
-svn export https://github.com/haiibo/packages/trunk/luci-app-onliner package/luci-app-onliner
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-onliner package/luci-app-onliner
 #sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
 #sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
 chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
@@ -154,4 +143,3 @@ find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/
 # sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/controller/*.lua
 # sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/*.lua
 # sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/view/v2ray_server/*.htm
-
