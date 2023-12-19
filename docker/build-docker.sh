9ca1caaa1cbddb714f2d1caf79520f1331051580
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-19 18:46:07 +0800

    生成hub docker的主要文件

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -5,6 +5,8 @@ if [ ! -z "$1" ];then
 	TAG=$1
 fi
 
+echo "TAG=$TAG" >> $GITHUB_ENV
+
 TMPDIR=openwrt_rootfs
 OUTDIR=opt/imgs
 IMG_NAME=mojialin/openwrt_onecloud
@@ -21,14 +23,3 @@ rm -f "$TMPDIR/etc/bench.log" && \
 docker build -t ${IMG_NAME}:${TAG} . && \
 rm -rf "$TMPDIR" && \
 docker save ${IMG_NAME}:${TAG} | pigz -9 > $OUTDIR/docker-img-openwrt-oncloud-${TAG}.gz
-
-gzip -dc $OUTDIR/*.gz | docker load
-docker login --username=${{ secrets.DOCKER_USERNAME }} --password=${{ secrets.DOCKER_PASSWORD }}
-
-docker tag mojialin/openwrt_onecloud mojialin/openwrt_onecloud:${TAG}
-docker push mojialin/openwrt_onecloud
-docker push mojialin/openwrt_onecloud:${TAG}
-
-cd ..
-mv hubdocker/opt/imgs/docker-img-openwrt-oncloud-${TAG}.gz openwrt/bin/targets/*/*/docker-img-openwrt-oncloud-${TAG}.gz
-echo "::set-output name=status::success"
