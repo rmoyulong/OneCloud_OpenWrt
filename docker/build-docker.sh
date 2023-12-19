ed7d4214319c280d38a00b3d4e017604c28affa8
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-20 01:51:57 +0800

    生成hub docker的主要文件

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -2,25 +2,23 @@
 
 TAG=latest
 echo "OWRT_TAG=$TAG" >> $GITHUB_ENV
+TMPDIR=openwrt_rootfs
+OUTDIR=opt/imgs
+IMG_NAME=mojialin/openwrt_onecloud
 		
 dos2unix ./*.sh
 dos2unix ./rc.local
 chmod +x ./*.sh
 chmod +x ./rc.local
 		
-TMPDIR=openwrt_rootfs
-OUTDIR=opt/imgs
-
 [ -d "$TMPDIR" ] && rm -rf "$TMPDIR"
 mkdir -p "$OUTDIR"
-mkdir -p "$TMPDIR"
-
-cd openwrt_rootfs
-tar zxvfp ../openwrt-armvirt-onecloud-rootfs.tar.gz
-cp ../rc.local etc/rc.local
-tar zcvfp ../openwrt-armvirt-onecloud-rootfs.tar.gz .
-
-cd ..
-docker build -t mojialin/openwrt_onecloud:${TAG} .
-rm -rf "$TMPDIR"  
-docker save mojialin/openwrt_onecloud:${TAG} | pigz -9 > $OUTDIR/docker-img-openwrt-oncloud-${TAG}.gz
+mkdir -p "$TMPDIR"  && \
+gzip -dc openwrt-armvirt-onecloud-rootfs.tar.gz | ( cd "$TMPDIR" && tar xf - ) && \
+cp -f rc.local "$TMPDIR/etc/" && \
+(cd "$TMPDIR" && tar cf ../openwrt-armvirt-onecloud-rootfs.tar .) && \
+rm -f DockerImg-OpenwrtArm32-${TAG}.gz && \
+docker build -t ${IMG_NAME}:${TAG} . && \
+rm -f  openwrt-armvirt-onecloud-rootfs.tar && \
+rm -rf "$TMPDIR" && \
+docker save ${IMG_NAME}:${TAG} | pigz -9 > $OUTDIR/docker-img-openwrt-oncloud-${TAG}.gz
