cc26f36ce34297a56e153511b7ba73104972ba97
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-19 22:12:14 +0800

    修改rootfs

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -10,12 +10,12 @@ mkdir -p "$TMPDIR"
 
 cd "$TMPDIR"
 tar zxvfp ../openwrt-armvirt-onecloud-rootfs.tar.gz
-mv $GITHUB_WORKSPACE/docker/rc.local ./etc/rc.local
+cp ../rc.local ./etc/rc.local
 
 tar zcvfp ../openwrt-armvirt-onecloud-rootfs.tar.gz .
 
 cd ..
 
-docker build -t ${IMG_NAME}:${{ env.OWRT_TAG }} .
+docker build -t mojialin/openwrt_onecloud:$OWRT_TAG .
 rm -rf "$TMPDIR" 
-docker save ${IMG_NAME}:${{ env.OWRT_TAG }} | pigz -9 > $OUTDIR/docker-img-openwrt-oncloud-${{ env.OWRT_TAG }}.gz
+docker save mojialin/openwrt_onecloud:$OWRT_TAG | pigz -9 > $OUTDIR/docker-img-openwrt-oncloud-$OWRT_TAG.gz
