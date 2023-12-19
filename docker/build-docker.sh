8f8e860257e802e11000d689a3cc9cbb9600ecd9
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-19 23:57:31 +0800

    修改rootfs

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -7,12 +7,12 @@ OUTDIR=opt/imgs
 mkdir -p "$OUTDIR"
 mkdir -p "$TMPDIR"
 
-cd /mnt/hubdocker/openwrt_rootfs
-tar zxvfp /mnt/hubdocker/openwrt-armvirt-onecloud-rootfs.tar.gz
-cp /mnt/hubdocker/rc.local /mnt/hubdocker/openwrt_rootfs/etc/rc.local
-tar zcvfp /mnt/hubdocker/openwrt-armvirt-onecloud-rootfs.tar.gz .
+cd /mnt/docker/openwrt_rootfs
+tar zxvfp /mnt/docker/openwrt-armvirt-onecloud-rootfs.tar.gz
+cp /mnt/docker/rc.local /mnt/docker/openwrt_rootfs/etc/rc.local
+tar zcvfp /mnt/docker/openwrt-armvirt-onecloud-rootfs.tar.gz .
 
-cd /mnt/hubdocker/
+cd /mnt/docker/
 docker build -t mojialin/openwrt_onecloud:${OWRT_TAG} .
 rm -rf "$TMPDIR"  
 docker save mojialin/openwrt_onecloud:${OWRT_TAG} | pigz -9 > $OUTDIR/docker-img-openwrt-oncloud-${OWRT_TAG}.gz
