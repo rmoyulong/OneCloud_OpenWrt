34f28bcf9a1a4ccd62bef1b768cbef88acfb31d1
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-19 23:31:40 +0800

    修改rootfs

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -5,10 +5,14 @@ OUTDIR=opt/imgs
 
 [ -d "$TMPDIR" ] && rm -rf "$TMPDIR"
 mkdir -p "$OUTDIR"
-mkdir -p "$TMPDIR"   && \
-gzip -dc openwrt-armvirt-onecloud-rootfs.tar.gz | ( cd "$TMPDIR" && tar xf - ) && \
-cp -f rc.local "$TMPDIR/etc/" && \
-(cd "$TMPDIR" && tar cf ../openwrt-armvirt-onecloud-rootfs.tar .) && \
-docker build -t mojialin/openwrt_onecloud:${OWRT_TAG} . && \
-rm -rf "$TMPDIR"  && \
+mkdir -p "$TMPDIR"
+
+cd /mnt/hubdocker/openwrt_rootfs
+tar zxvfp /mnt/hubdocker/openwrt-armvirt-onecloud-rootfs.tar.gz
+cp /mnt/hubdocker/rc.local /mnt/hubdocker/openwrt_rootfs/etc/rc.local
+tar zcvfp /mnt/hubdocker/openwrt-armvirt-onecloud-rootfs.tar.gz .
+
+cd /mnt/hubdocker/
+docker build -t mojialin/openwrt_onecloud:${OWRT_TAG} .
+rm -rf "$TMPDIR"  
 docker save mojialin/openwrt_onecloud:${OWRT_TAG} | pigz -9 > $OUTDIR/docker-img-openwrt-oncloud-${OWRT_TAG}.gz
