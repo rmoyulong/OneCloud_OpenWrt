7bd357436f027077c8f81549e52a403d271171cb
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-23 00:36:14 +0800

    Add files via upload

--- onecloud/files/etc/1.sh
+++ onecloud/files/etc/1.sh
@@ -8,7 +8,7 @@ resize2fs /dev/mmcblk1p2
 echo "# Put your custom commands here that should be executed once
 # the system init finished. By default this file does nothing.
 
-mount -o remount rw /
+#mount -o remount rw /
 exit 0">/etc/rc.local
 rm -rf /etc/1.sh
 
