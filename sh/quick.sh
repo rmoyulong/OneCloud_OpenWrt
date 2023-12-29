a2c463f9b3a679ba801bfae1b5debca96b1bf553
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-29 16:56:40 +0800

    一站式和普通image以及需要删除插件的订阅文件分开建立

--- sh/quick.sh
+++ sh/quick.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# 移除要替换的包
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/msd_lite
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-netgear
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-serverchan
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-homeproxy
+
 #设置armv7 32位的openclash
 mkdir -p files/etc/openclash/core
 mkdir -p files/etc/profile.d
