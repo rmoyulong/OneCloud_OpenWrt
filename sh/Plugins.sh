44d57a64b10e56dd626cfef5738384d8ce11531a
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-12 22:17:08 +0800

    snv失效后，合并订阅源kiddin9

---
+++ sh/Plugins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 移除要替换的包
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/msd_lite
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-netgear
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-serverchan
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-homeproxy
+
+#echo 'src-git kenzo8 https://github.com/kenzok8/small-package' >> feeds.conf.default
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >> feeds.conf.default
