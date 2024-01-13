61769f807aa460de9c14493085660db2c81f47f1
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-13 21:14:01 +0800

    snv失效，合并插件订阅源

---
+++ sh/tiny.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 移除要替换的包
+rm -rf feeds/packages/net/sing-box
+rm -rf feeds/packages/net/chinadns-ng
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/msd_lite
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-netgear
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-serverchan
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-homeproxy
+
+# 科学上网插件
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git mo_app https://github.com/rmoyulong/mo_app' >> feeds.conf.default
