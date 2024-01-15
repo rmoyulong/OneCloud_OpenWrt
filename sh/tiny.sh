702519fe4be016d2d84236d474fef5edd17599f4
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-15 15:05:47 +0800

    snv失效，合并插件订阅源

--- sh/tiny.sh
+++ sh/tiny.sh
@@ -15,7 +15,7 @@ rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-homeproxy
 
 # 科学上网插件
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+#git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 echo 'src-git kenzok8 https://github.com/kenzok8/small-package' >> feeds.conf.default
 echo 'src-git mo_app https://github.com/rmoyulong/mo_app' >> feeds.conf.default
 
