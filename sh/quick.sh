dd01c5536ab67a00400670a799121da8553e2492
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-22 04:25:07 +0800

    snv模拟订阅源

--- sh/quick.sh
+++ sh/quick.sh
@@ -45,22 +45,6 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-merge_package(){
-    # 参数1是分支名,参数2是库地址,参数3是子路径。所有文件下载到openwrt/package/openwrt-packages路径。
-    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
-    trap 'rm -rf "$tmpdir"' EXIT
-    branch=$1 curl=$2 && shift 2
-    rootdir=$(pwd)
-    localdir=package/openwrt-packages
-    [ -d $localdir ] || mkdir -p $localdir
-    tmpdir=$(mktemp -d) || exit 1
-    git clone -b $branch --depth 1 --filter=blob:none --sparse $curl $tmpdir
-    cd $tmpdir
-    git sparse-checkout init --cone
-    git sparse-checkout set $@
-    mv -f $@ $rootdir/$localdir && cd $rootdir
-}
-
 # 添加额外插件
 git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
@@ -68,7 +52,6 @@ git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
-merge_package master https://github.com/lisaac/luci-app-dockerman applications/luci-app-dockerman
 
 git_sparse_clone main https://github.com/kenzok8/small-package luci-app-filebrowser
 git_sparse_clone main https://github.com/kenzok8/small-package luci-app-ssr-mudb-server
@@ -94,17 +77,24 @@ git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-eas
 
 # 科学上网插件
 git clone --depth=1 https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
-git clone --depth=1 -b master https://github.com/kenzok8/small package/small
-#git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
-
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-bypass
 
 # 科学上网插件依赖
+git_sparse_clone main https://github.com/kenzok8/small-package v2dat
 git_sparse_clone main https://github.com/kenzok8/small-package lua-maxminddb
+git_sparse_clone main https://github.com/kenzok8/small-package brook
+git_sparse_clone main https://github.com/kenzok8/small-package trojan-go
+git_sparse_clone main https://github.com/kenzok8/small-package trojan-plus
 git_sparse_clone main https://github.com/kenzok8/small-package sagernet-core
+git_sparse_clone main https://github.com/kenzok8/small-package lua-neturl
+git_sparse_clone main https://github.com/kenzok8/small-package redsocks2
+git_sparse_clone main https://github.com/kenzok8/small-package aria2
+git_sparse_clone main https://github.com/kenzok8/small-package ariang
 git_sparse_clone main https://github.com/kenzok8/small-package filebrowser
 git_sparse_clone main https://github.com/kenzok8/small-package qBittorrent-Enhanced-Edition
 git_sparse_clone main https://github.com/kenzok8/small-package qBittorrent-static
@@ -114,6 +104,7 @@ git_sparse_clone main https://github.com/kenzok8/small-package qtbase
 git_sparse_clone main https://github.com/kenzok8/small-package qttools
 git_sparse_clone main https://github.com/kenzok8/small-package rblibtorrent
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages vlmcsd
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages sing-box
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages UnblockNeteaseMusic-Go
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages UnblockNeteaseMusic
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages wrtbwmon
@@ -124,20 +115,19 @@ git_sparse_clone master https://github.com/kiddin9/openwrt-packages n2n
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages adbyby
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages dns-forwarder
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages upx
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages ucl
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages aria2
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages ariang
 
 # Themes
-git_sparse_clone master https://github.com/kenzok8/luci-theme-ifit luci-theme-ifit 
+git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+git_sparse_clone main https://github.com/kenzok8/small-package luci-theme-ifit 
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 
 # 晶晨宝盒
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-amlogic
-git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
-sed -i "/amlogic_firmware_repo/ { s|https://github.com/breakings/OpenWrt|https://github.com/rmoyulong/rootfs_builder|g }" package/luci-app-amlogic/root/etc/config/amlogic
-sed -i "s|ARMv8|4jarmv8|g" package/luci-app-amlogic/root/etc/config/amlogic
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-amlogic
+#sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/ophub/luci-app-amlogic'|g" package/luci-app-amlogic/root/etc/config/amlogic
+#sed -i "s|kernel_path.*|kernel_path 'https://github.com/ophub/kernel'|g" package/luci-app-amlogic/root/etc/config/amlogic
+#sed -i "s|ARMv8|ARMv8_PLUS|g" package/luci-app-amlogic/root/etc/config/amlogic
 
 # SmartDNS
 git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
@@ -148,16 +138,18 @@ git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-a
 git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
 
 # MosDNS
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-mosdns
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages mosdns
+#git_sparse_clone main https://github.com/kenzok8/small-package luci-app-mosdns
+#git_sparse_clone main https://github.com/kenzok8/small-package mosdns
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 
 # DDNS.to
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-ddnsto
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages ddnsto
+git_sparse_clone main https://github.com/kenzok8/small-package luci-app-ddnsto
+git_sparse_clone main https://github.com/kenzok8/small-package ddnsto
 
 # Alist
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-alist
-git_sparse_clone main https://github.com/kenzok8/small-package alist
+#git_sparse_clone main https://github.com/kenzok8/small-package luci-app-alist
+#git_sparse_clone main https://github.com/kenzok8/small-package alist
+git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
 
 # iStore
 #git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
@@ -171,7 +163,7 @@ git_sparse_clone master https://github.com/kiddin9/openwrt-packages taskd
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-onliner
 #sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
 #sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
-sudo chmod -Rf 755 package/luci-app-onliner
+chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
 
 # 取消主题默认设置
 find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
