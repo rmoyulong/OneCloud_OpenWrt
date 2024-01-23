bb5c40e5d54238f49b4b2d9e40227933f8b8025c
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-24 01:25:20 +0800

    immortalwrt库专用订阅源

--- sh/immortalwrt.sh
+++ sh/immortalwrt.sh
@@ -14,7 +14,7 @@ rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-netgear
 rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/applications/luci-app-serverchan
-rm -rf feeds/luci/applications/luci-app-homeproxy
+#rm -rf feeds/luci/applications/luci-app-homeproxy
 rm -rf feeds/luci/applications/luci-app-openclash
 
 # Git稀疏克隆，只克隆指定目录到本地
