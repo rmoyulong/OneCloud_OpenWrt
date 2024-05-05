3f40f9d4f0eab0f94e15fe01d2ab41b49d810206
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-05 22:16:06 +0800

    删除冲突的插件

--- sh/remove.sh
+++ sh/remove.sh
@@ -1,6 +1,6 @@
-rm -rf feeds/packages/net/aria2
-rm -rf feeds/packages/net/ariang
-rm -rf feeds/packages/net/mosdns
+#rm -rf feeds/packages/net/aria2
+#rm -rf feeds/packages/net/ariang
+#rm -rf feeds/packages/net/mosdns
 rm -rf feeds/luci/applications/luci-app-aria2
 rm -rf feeds/luci/applications/luci-app-bypass
 rm -rf feeds/luci/applications/luci-app-mosdns
