123497541e705ff4f7b08fa9688a9cab1af92561
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-06 13:29:42 +0800

    immortalwrt库配套的源

--- sh/immortalwrt.sh
+++ sh/immortalwrt.sh
@@ -16,6 +16,7 @@ rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/applications/luci-app-serverchan
 #rm -rf feeds/luci/applications/luci-app-homeproxy
 rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-amlogic
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-passwall2
 rm -rf feeds/luci/applications/luci-app-turboacc
@@ -36,8 +37,8 @@ echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 #echo 'src-git my_luci https://github.com/rmoyulong/my_luci' >> feeds.conf.default
 #echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy' >> feeds.conf.default
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages aria2
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages ariang
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages aria2
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages ariang
 #it_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-vlmcsd
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-kodexplorer
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-usb-printer
