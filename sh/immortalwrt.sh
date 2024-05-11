7ed4f876b53b2efa67473d54c6b093a4f0f5f0c2
GitHub <noreply@github.com>
2024-05-11 21:11:16 +0800

    修正hub docker以及aria2错误 2024-05-11

--- sh/immortalwrt.sh
+++ sh/immortalwrt.sh
@@ -46,12 +46,11 @@ echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede aria2
 git_sparse_clone main https://github.com/kenzok8/small-package ariang
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede zerotier
-git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede luci-app-zerotier
-#it_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-vlmcsd
+git_sparse_clone master https://github.com/kiddin9/openwrt-packagesluci-app-zerotier
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-vlmcsd
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-kodexplorer
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-usb-printer
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-turboacc
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packagesluci-app-zerotier
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-ssr-plus
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadowsocksr-libev
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-bypass
