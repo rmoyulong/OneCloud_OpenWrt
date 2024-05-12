55b484fca31da1849bb81cde567d74f49e57f6d7
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-12 16:06:39 +0800

    immortalwrt扩展库

--- sh/immortalwrt.sh
+++ sh/immortalwrt.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
 
 # 移除要替换的包
-rm -rf feeds/packages/net/alist
-rm -rf feeds/packages/net/aria2
-rm -rf feeds/packages/net/ariang
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/msd_lite
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/applications/luci-app-msd_lite
-rm -rf feeds/luci/applications/luci-app-smartdns
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-netgear
-rm -rf feeds/luci/applications/luci-app-netdata
-rm -rf feeds/luci/applications/luci-app-serverchan
-#rm -rf feeds/luci/applications/luci-app-homeproxy
-rm -rf feeds/luci/applications/luci-app-openclash
-rm -rf feeds/luci/applications/luci-app-amlogic
-rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-passwall2
-rm -rf feeds/luci/applications/luci-app-turboacc
-#rm -rf feeds/packages/net/shadowsocks-libev
+rm -rf ./feeds
 
 #aria2
 chmod -R 777 $GITHUB_WORKSPACE/$OWRT_FILES/etc/aria2
