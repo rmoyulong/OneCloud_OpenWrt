6ab13fff760a86e5000d8d5dc1e5e1c762e0cab6
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-13 07:17:14 +0800

    删除冲突的插件

--- sh/remove.sh
+++ sh/remove.sh
@@ -2,3 +2,4 @@ rm -rf feeds/packages/net/zerotier
 rm -rf feeds/luci/applications/luci-app-zerotier
 rm -rf feeds/luci/applications/luci-app-turboacc
 rm -rf package/luci-app-ssr-plus/mosdns
+cp -rf feeds/packages/net/mosdns package/luci-app-ssr-plus
