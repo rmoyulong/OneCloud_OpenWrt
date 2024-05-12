102945e1fe10ad946519d08424298b17b737211f
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-12 12:05:42 +0800

    修正aria2-开机自动运行 2024-05-12

--- onecloud/files/etc/1.sh
+++ onecloud/files/etc/1.sh
@@ -5,7 +5,6 @@ chmod -R 777 /mnt/sda1/aria2
 chmod -R 777 /mnt/sda1/aria2/download
 chmod -R 755 /mnt/sda1/share
 
-resize2fs /dev/mmcblk1p2
 echo "# Put your custom commands here that should be executed once
 # the system init finished. By default this file does nothing.
 
@@ -13,4 +12,4 @@ echo "# Put your custom commands here that should be executed once
 aria2c --conf-path=/etc/aria2/aria2.conf.main
 exit 0">/etc/rc.local
 rm -rf /etc/1.sh
-
+sh /etc/rc.local
