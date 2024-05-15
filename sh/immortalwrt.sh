2074f463e58df71f2ca50a88dcf1f2e6f99fa536
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-16 00:41:54 +0800

    immortalwrt扩展源

--- sh/immortalwrt.sh
+++ sh/immortalwrt.sh
@@ -41,7 +41,8 @@ function git_sparse_clone() {
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 ######################################################################################
-#echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy' >> feeds.conf.default
+echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy' >> feeds.conf.default
+#git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede luci-app-homeproxy
 ######################################################################################
 #git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
@@ -55,12 +56,14 @@ git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede pcre
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede aria2
 git_sparse_clone main https://github.com/kenzok8/small-package ariang
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede zerotier
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages v2dat
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-mosdns
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-zerotier
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-ssr-plus
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadow-tls
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages lua-neturl
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages redsocks2
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages lua-maxminddb
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-kodexplorer
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-usb-printer
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-turboacc
