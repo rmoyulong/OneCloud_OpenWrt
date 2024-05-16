88e86b55f61c8878d206db024fe456f10007a83d
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-16 16:08:01 +0800

    immortalwrt扩展源

--- sh/immortalwrt.sh
+++ sh/immortalwrt.sh
@@ -41,8 +41,9 @@ function git_sparse_clone() {
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 ######################################################################################
-echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy' >> feeds.conf.default
-#git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede luci-app-homeproxy
+#echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy' >> feeds.conf.default
+git clone --depth=1 https://github.com/immortalwrt/homeproxy package/homeproxy
+git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede luci-app-homeproxy
 ######################################################################################
 #git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
