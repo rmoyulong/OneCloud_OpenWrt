0f55e60110259329b008addbcc78317fd6d213f2
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-16 16:08:23 +0800

    删除冲突的插件

--- sh/remove.sh
+++ sh/remove.sh
@@ -3,3 +3,5 @@ rm -rf feeds/luci/applications/luci-app-zerotier
 rm -rf feeds/luci/applications/luci-app-turboacc
 rm -rf feeds/kenzo/homeproxy
 rm -rf feeds/kenzo/luci-app-homeproxy
+rm -rf feeds/packages/net/homeproxy
+rm -rf feeds/luci/applications/luci-app-homeproxy
