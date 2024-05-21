8748762d432fd62dc72471d14a29ce93c214f8fc
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-21 23:51:06 +0800

    增加和删除扩展源

--- sh/immortalwrt.sh
+++ sh/immortalwrt.sh
@@ -50,9 +50,6 @@ git_sparse_clone master https://github.com/kiddin9/openwrt-packages dnsforwarder
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede pcre
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede aria2
 git_sparse_clone main https://github.com/kenzok8/small-package ariang
-#git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede ztdns
-#git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede libnatpmp
-#git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede zerotier
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-zerotier
 git_sparse_clone main https://github.com/kenzok8/small-package luci-app-kodexplorer
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-mosdns
