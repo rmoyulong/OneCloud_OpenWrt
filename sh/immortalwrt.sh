65858702e698eafa9ccf1e7579721739cc246ee6
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-21 21:50:07 +0800

    增加和删除扩展源

--- sh/immortalwrt.sh
+++ sh/immortalwrt.sh
@@ -49,11 +49,12 @@ git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-byp
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages dnsforwarder
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede pcre
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede aria2
+git_sparse_clone main https://github.com/kenzok8/small-package ariang
 #git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede ztdns
 #git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede libnatpmp
 #git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede zerotier
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-zerotier
-git_sparse_clone main https://github.com/kenzok8/small-package ariang
+git_sparse_clone main https://github.com/kenzok8/small-package luci-app-kodexplorer
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-mosdns
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-ssr-plus
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages v2dat
@@ -61,7 +62,6 @@ git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadow-tls
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages lua-neturl
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages redsocks2
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages lua-maxminddb
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-kodexplorer
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-usb-printer
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-turboacc
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadowsocksr-libev
