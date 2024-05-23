5af46d5f7444cc693216e653ee6dcb9bfdb730d6
GitHub <noreply@github.com>
2024-05-23 13:38:59 +0800

    aria2 可道云 zerotier homeproxy启动 2024-05-23

--- onecloud/files/etc/1.sh
+++ onecloud/files/etc/1.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 chmod -R 777 /etc/aria2
 chmod -R 777 /mnt/sda1/aria2
 chmod -R 777 /mnt/sda1/aria2/download
@@ -9,9 +7,9 @@ echo "# Put your custom commands here that should be executed once
 # the system init finished. By default this file does nothing.
 
 #mount -o remount rw /
-aria2c --conf-path=/etc/aria2/aria2.conf.main
-/etc/init.d/kodexplorer start
-/etc/init.d/zerotier start
+/usr/sbin/balethirq.pl
+sh /etc/diymo.sh
+aria2c --conf-path=/etc/aria2/aria2.conf.main >/dev/null 2>&1
 exit 0">/etc/rc.local
 rm -rf /etc/1.sh
 sh /etc/rc.local
---
+++ onecloud/files/etc/diymo.sh
@@ -0,0 +1,6 @@
+/etc/init.d/kodexplorer restart
+/etc/init.d/zerotier restart
+/etc/init.d/samba4 restart
+/etc/init.d/ttyd restart
+/etc/init.d/homeproxy restart
+
