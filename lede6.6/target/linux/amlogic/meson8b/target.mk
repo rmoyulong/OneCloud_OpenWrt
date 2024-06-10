bafb6a932cc49a627842cdfa335e61d92533f6ef
rmoyulong <mojialin99@qq.com>
2024-06-10 13:50:53 +0800

    精简玩客云SOC参数 2024-06-10

--- lede6.6/target/linux/amlogic/meson8b/target.mk
+++ lede6.6/target/linux/amlogic/meson8b/target.mk
@@ -1,12 +1,8 @@
-# SPDX-License-Identifier: GPL-2.0-only
-#
-# Copyright (C) 2006-2013 OpenWrt.org
-
 ARCH:=arm
 SUBTARGET:=meson8b
 BOARDNAME:=Amlogic S805 boards (32 bit)
 CPU_TYPE:=cortex-a5
-CPU_SUBTYPE:=neon-vfpv4
+CPU_SUBTYPE:=vfpv4
 
 define Target/Description
 	Build firmware image for Amlogic S805 SoC devices.
