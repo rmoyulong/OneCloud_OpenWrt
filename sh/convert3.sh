50e32b7368c7dc4724a4f2a8f4541b7984bd229e
rmoyulong <mojialin99@qq.com>
2025-05-04 18:58:20 +0800

    提交玩客云6.11内核融合版

---
+++ sh/convert3.sh
@@ -0,0 +1,52 @@
+sudo apt install android-sdk-libsparse-utils
+ver="v0.3.2"
+curl -L -o ./AmlImg https://github.com/rmoyulong/AmlImg/releases/download/$ver/AmlImg_${ver}_linux_amd64
+chmod +x ./AmlImg
+curl -L -o ./uboot.img https://github.com/rmoyulong/u-boot-onecloud/releases/download/Onecloud_Uboot_23.12.24_18.15.09/eMMC.burn.img
+./AmlImg unpack ./uboot.img burn/
+
+gunzip openwrt/bin/targets/*/*/*.gz
+diskimg=$(ls openwrt/bin/targets/*/*/*.img)
+loop=$(sudo losetup --find --show --partscan $diskimg)
+img_ext="openwrt.img"
+img_mnt="xd"
+rootfs_mnt="img"
+boot_img=${img_ext}
+boot_img_mnt=${img_mnt}
+rootfs_img_mnt=${rootfs_mnt}
+echo ${boot_img}
+echo ${boot_img_mnt}
+echo ${rootfs_img_mnt}
+sudo rm -rf ${boot_img}
+sudo rm -rf ${boot_img_mnt}
+sudo rm -rf ${rootfs_img_mnt}
+sudo dd if=/dev/zero of=${boot_img} bs=1M count=2000
+sudo mkfs.ext4 ${boot_img}
+sudo mkdir ${boot_img_mnt}
+sudo mkdir ${rootfs_img_mnt}
+sudo mount ${boot_img} ${boot_img_mnt}
+sudo mount ${loop}p2 ${rootfs_img_mnt}
+cd ${rootfs_img_mnt}
+sudo cp -r * ../${boot_img_mnt}
+cd ..
+sudo sync
+sudo umount ${boot_img_mnt}
+sudo umount ${rootfs_img_mnt}
+#sudo img2simg ${loop}p1 burn/boot.simg
+mv $GITHUB_WORKSPACE/lede6.11/boot.PARTITION burn/boot.simg
+sudo img2simg openwrt.img burn/rootfs.simg
+sudo rm -rf *.img
+sudo losetup -d $loop
+cat <<EOF >>burn/commands.txt
+PARTITION:boot:sparse:boot.simg
+PARTITION:rootfs:sparse:rootfs.simg
+EOF
+prefix=$(ls openwrt/bin/targets/*/*/*.img | sed 's/\.img$//')
+burnimg=${prefix}.burn.img
+./AmlImg pack $burnimg burn/
+for f in openwrt/bin/targets/*/*/*.burn.img; do
+  sha256sum "$f" >"${f}.sha"
+  xz -9 --threads=0 --compress "$f"
+done
+sudo rm -rf openwrt/bin/targets/*/*/*.img
+sudo rm -rf openwrt/bin/targets/*/*/*.gz
