39fbc8d0156211b7fe6d8f630d74e6afd0f4c675
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-10-11 18:51:26 +0800

    old python3 patch

--- sh/python3.patch.sh
+++ sh/python3.patch.sh
@@ -2,6 +2,6 @@
 source $GITHUB_WORKSPACE/sh/functions.sh
 
 cd openwrt
-merge_package master https://github.com/coolsnowwolf/packages package lang/python
+merge_package master https://github.com/rmoyulong/old_coolsnowwolf_packages package lang/python
 rm -rf feeds/packages/lang/python
 cp -rf ./package/python feeds/packages/lang/
