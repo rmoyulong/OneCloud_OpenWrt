a3e8e5630d6d2dad75813d8f7e1924696ac6f9a6
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-03-04 21:09:04 +0800

    自定义插件包

--- sh/setup.sh
+++ sh/setup.sh
@@ -1,11 +1,11 @@
 #公用函数
 source $GITHUB_WORKSPACE/sh/functions.sh
 
-rm -rf feeds/packages/lang/rust
 merge_package master https://github.com/coolsnowwolf/packages package lang/rust
-
 git clone https://github.com/stupidloud/helloworld package/helloworld
-rm -rf feeds/packages/net/xray-core
+
+rm -rf package/helloworld/v2ray-geodata
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 cd package
 $GITHUB_WORKSPACE/sh/Packages.sh
