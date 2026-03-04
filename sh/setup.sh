690d8e3601eb6e17b4ed0c7e636f2e36c40aa346
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-03-04 23:10:44 +0800

    自定义插件包

--- sh/setup.sh
+++ sh/setup.sh
@@ -7,6 +7,10 @@ git clone https://github.com/stupidloud/helloworld package/helloworld
 rm -rf package/helloworld/v2ray-geodata
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
+
+rm -rf package/helloworld/shadowsocks-rust
+merge_package master https://github.com/kenzok8/small/ package shadowsocks-rust
+
 cd package
 $GITHUB_WORKSPACE/sh/Packages.sh
 $GITHUB_WORKSPACE/sh/Handles.sh
