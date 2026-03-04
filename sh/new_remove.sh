42843ab244e39712481622f71ea78e94a92dd31d
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-03-05 03:43:08 +0800

    删除冲突的插件和依赖

--- sh/new_remove.sh
+++ sh/new_remove.sh
@@ -1,2 +1,4 @@
 rm -rf feeds/packages/lang/rust
 rm -rf feeds/packages/net/xray-core
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
