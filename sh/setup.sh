c520601918a39e1696f84ad28d5625b0221b2889
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-05-18 21:15:01 +0800

    自定义插件包

--- sh/setup.sh
+++ sh/setup.sh
@@ -2,24 +2,27 @@
 source $GITHUB_WORKSPACE/sh/functions.sh
 
 merge_package master https://github.com/coolsnowwolf/packages package lang/rust
-git clone https://github.com/stupidloud/helloworld package/helloworld
+#git clone https://github.com/stupidloud/helloworld package/helloworld
 
 rm -rf package/helloworld/v2ray-geodata
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 
-rm -rf package/helloworld/shadowsocks-rust
 merge_package master https://github.com/kenzok8/small/ package shadowsocks-rust
+merge_package master https://github.com/kenzok8/small/ package shadowsocks-libev
+merge_package master https://github.com/kenzok8/small/ package shadowsocksr-libev
 merge_package master https://github.com/kenzok8/small/ package sing-box
 merge_package master https://github.com/kenzok8/small/ package luci-app-ssr-plus
-
-rm -rf package/helloworld/xray-core
-rm -rf package/helloworld/geoview
 merge_package master https://github.com/kenzok8/small/ package xray-core
 merge_package master https://github.com/kenzok8/small/ package geoview
 
 rm -rf feeds/luci/applications/luci-app-homeproxy
 git clone https://github.com/VIKINGYFY/homeproxy package/luci-app-homeproxy
+
+rm -rf feeds/luci/applications/luci-app-amlogic
+rm -rf package/luci-app-amlogic
+git clone -b main https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+sed -i "s|ARMv8|RELEASES_TAGS_KEYWORD|g" package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
 
 cd package
 $GITHUB_WORKSPACE/sh/Packages.sh
